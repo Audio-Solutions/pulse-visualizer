92cd112b650d515bf0599c97171dec0febf18ac2
Beacroxx <nightmare091105@gmail.com>
2025-06-19 22:54:24 +0200

    remove unused function declaration

--- src/audio_processing.hpp
+++ src/audio_processing.hpp
@@ -6,8 +6,6 @@
 
 namespace AudioProcessing {
 // Signal processing functions
-void applyBandpass(const std::vector<float>& input, std::vector<float>& output, float centerFreq, float sampleRate,
-                   float bandwidth = 10.0f);
 
 namespace Butterworth {
 // Apply bandpass filter to circular buffer in temporal order
