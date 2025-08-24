113241189888ea8d4aed72feaeb387d4aac4c28e
Beacroxx <nightmare091105@gmail.com>
2025-08-24 15:28:36 +0200

    fix 1:1 mapping without curvature being pixely due to floor instead of round

--- src/window_manager.cpp
+++ src/window_manager.cpp
@@ -238,8 +238,8 @@ void VisualizerWindow::transferTexture(GLuint oldTex, GLuint newTex, GLenum form
   glTexImage2D(GL_TEXTURE_2D, 0, format == GL_RED_INTEGER ? GL_R32UI : GL_RGBA8, width,
                SDLWindow::windowSizes[sdlWindow].second, 0, format, type, newData.data());
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
